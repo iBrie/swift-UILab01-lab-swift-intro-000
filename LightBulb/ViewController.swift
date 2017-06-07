86009958511fcc053ab16a9069e6bc366d5cd505
iBrie <ibrigitte@me.com>
2017-06-07 21:33:10 +0200

    Completed Bulb

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -12,20 +12,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lightBulb: UIImageView!
     
-    
+  
+  
+  
     override func viewDidLoad() {
-        
+      
+      
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
+      lightBulb.backgroundColor = UIColor.red
         
     }
     
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
+      
+      
+      switch sender.selectedSegmentIndex {
+      case 0:
+        lightBulb.backgroundColor = UIColor.red
+      case 1:
+        lightBulb.backgroundColor = UIColor.yellow
+      case 2:
+        lightBulb.backgroundColor = UIColor.blue
+      case 3:
+        lightBulb.backgroundColor = UIColor.green
+      default:
+          lightBulb.backgroundColor = UIColor.black
         print(sender.selectedSegmentIndex)
-        
+      
+      }
+    
     }
     
     
